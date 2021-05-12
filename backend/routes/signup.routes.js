eef775dcfce2cde95e14399f4440fb6f397ae34b
sulupr <71424933+sulupr@users.noreply.github.com>
2021-05-12 17:53:19 +0530

    Delete backend directory

--- backend/routes/signup.routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-    const user = require("../controllers/auth.js");
-  
-    var router = require("express").Router();
-  
-    // Create a new Tutorial
-    router.post("/", user.signup);
-};
